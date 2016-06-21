6475310b315cd284dcd1a7d1643ae8019b418ffc
ppstacy <ppstacy@gmail.com>
2016-06-20 19:17:08 -0700

    [204. Count Primes][Accepted]committed by LeetCode Extension

--- 204-Count-Primes/solution_xrange.py
+++ 204-Count-Primes/solution_xrange.py
@@ -1,27 +1,24 @@
 
 # Time:  O(n)
 # Space: O(n)
+
 from math import sqrt
 
-class Solution(object):
-     def countPrimes(self, n):
-        """
-         :type n: int
-         :rtype: int
-         """
-        if n<=2:
+class Solution:
+    # @param {integer} n
+    # @return {integer}
+    def countPrimes(self, n):
+        if n <= 2:
             return 0
         
-        isPrimes= [True]*n
-        isPrimes[0],isPrimes[1] = False, False
+        is_prime = [True] * n
+        sqr = sqrt(n - 1)
         
-        num = 0 
-        sqr = sqrt(n-1)
-        
-        for i in xrange(2,sqr):
-               if isPrimes[i]:
-                   num += 1
-                   for j in xrange ( i +1, n, i):
-                       isPrimes[j]= False
-                       
+        num = 0
+        for i in xrange(2, n):
+            if is_prime[i]:
+               num += 1
+               for j in xrange(i+i, n, i):
+                   is_prime[j] = False
+                   
         return num
