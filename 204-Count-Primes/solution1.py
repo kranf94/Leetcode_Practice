05ad0ca968d19e0ea0dd5fd84d73fbdd3fdd3285
ppstacy <ppstacy@gmail.com>
2016-06-20 18:51:46 -0700

    [204. Count Primes][Runtime Error]committed by LeetCode Extension

---
+++ 204-Count-Primes/solution1.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def countPrimes(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n<=2
+        return 0;
+        
+        isPrimes= [True]*n
+        isPrimes[0],isPrimes[1] = False, False
+        x=2
+        while x*x<n
+              if isPrimes[x]:
+                  p=x*x
+                  while p<n
+                  isPrimes[p]= False
+                  p=x+p
+              x=x+1   
+       return sum(isPrimes)
+       
