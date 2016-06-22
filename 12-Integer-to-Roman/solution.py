62c618d92816971cf5c0e02a1dcfea40a18b1b02
ppstacy <ppstacy@gmail.com>
2016-06-21 18:11:44 -0700

    [12. Integer to Roman][Runtime Error]committed by LeetCode Extension

---
+++ 12-Integer-to-Roman/solution.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def intToRoman(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+        roman_num= { 1000,900,500,400,100,90,50,40,10,9,5,4,1}
+        roman_list= { "M","CM","D","CD","C","XC","L","XL" ,"X","IX","V","IV","I"}
+        
+        list = ''
+        for i in range (0,len(roman_num)) :
+            if num >= roman_num[i]:
+               num -= roman_num[i]
+            list += roman_list[i]
+        return list
+        
