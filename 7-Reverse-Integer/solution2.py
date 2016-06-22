dedb227d94d943451b6a73ed46a7bd65ed2972f2
ppstacy <ppstacy@gmail.com>
2016-06-21 19:08:35 -0700

    [7. Reverse Integer][Accepted]committed by LeetCode Extension

---
+++ 7-Reverse-Integer/solution2.py
@@ -0,0 +1,15 @@
+# Time: O(logn)
+# Space: O(1)
+
+class Solution:
+    # @return an integer
+ 
+    
+      def reverse(self, x):
+        answer = 0
+        sign = 1 if x > 0 else -1
+        x = abs(x)
+        while x > 0:
+            answer = answer * 10 + x % 10
+            x /= 10
+        return sign*answer if answer <= 2147483647 else 0
