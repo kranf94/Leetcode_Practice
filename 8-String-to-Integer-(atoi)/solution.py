9b64902c266a6071f53f9a53112b3dcfc1eaca3a
ppstacy <ppstacy@gmail.com>
2016-06-22 20:01:28 -0700

    [8. String to Integer (atoi)][Runtime Error]committed by LeetCode Extension

--- 8-String-to-Integer-(atoi)/solution.py
+++ 8-String-to-Integer-(atoi)/solution.py
@@ -12,20 +12,20 @@ class Solution(object):
         INT_MIN= -1*INT_MAX -1
         
         result = 0
-       while i < len(str) and str[i] == " " 
+        while i < len(str) and str[i] == " " 
              i += 1
         
-       sign =1
-       if str[i] == "+" :
-          i +=1
-       else str[i] == "-":
-          i += 1
-          sign = -1
-          
-       while i< len (str) and str[i] >='0' and str[i] <='9' :
-           if result > (INT_MAX - (ord(str[i]) -ord(str[0])))/10     ###返回对应的ASCII数值
-              return INT_MAX if sign >0 else INT_MIN
-           result =result*10 +ord(str[i]) -ord(str[0])
+        sign =1
+        if str[i] == "+" :
            i +=1
+        else str[i] == "-":
+           i += 1
+           sign = -1
+          
+        while i< len (str) and str[i] >='0' and str[i] <='9' :
+            if result > (INT_MAX - (ord(str[i]) -ord(str[0])))/10     ###返回对应的ASCII数值
+               return INT_MAX if sign >0 else INT_MIN
+            result =result*10 +ord(str[i]) -ord(str[0])
+            i +=1
         return sign*result
         
