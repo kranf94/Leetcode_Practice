a9dcf3fe399d952a3873bd4962ee96327346c4d5
ppstacy <ppstacy@gmail.com>
2016-06-23 09:08:27 -0700

    [36. Valid Sudoku][Accepted]committed by LeetCode Extension

--- 36-Valid-Sudoku/solution.py
+++ 36-Valid-Sudoku/solution.py
@@ -10,23 +10,24 @@ class Solution(object):
         row= [set([]) for i in range(9)]
         col= [set([]) for i in range(9)]
         grid= [set([]) for i in range(9)]
-        
-      
+       
         for r in range(9) :
-           for c in range (9) :
+            for c in range (9) :
                 if board[r][c] == "." :
-                   continue
+                    continue
                 if board[r][c] in row[r] :
-                   return False
+                    return False
                 if board[r][c] in col[c] :
-                   return False
+                    return False
                 
-                g= r/3*3+c/3
+                g= r / 3 * 3+ c / 3
                 if board[r][c] in grid[g]:
-                   return False
+                    return False
                 
                 grid[g].add(board[r][c])
                 row[r].add(board[r][c])
                 col[c].add(board[r][c])
                 
-         return True
+        return True
+        
+        
