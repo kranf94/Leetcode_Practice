42697727e98b420ece5b26f8d6e94ab709c59c3d
ppstacy <ppstacy@gmail.com>
2016-07-26 11:19:40 -0700

    [168. Excel Sheet Column Title][Accepted]committed by LeetCode Extension

--- 168-Excel-Sheet-Column-Title/solution2.py
+++ 168-Excel-Sheet-Column-Title/solution2.py
@@ -4,13 +4,12 @@ class Solution(object):
         :type n: int
         :rtype: str
         """
-        result =''
-        while n>0 :
-          
-                result = chr(ord('A') + (n-1) %26) +result
-                n = (n -1 )/26
-                
-        return result;
+   
                
     #n=26 n-1=25 直接避免了第一种方法里面 ==0 需要调取Z 的情况
+        result = ''
+        while n>0 :
+            result = chr(ord('A')+(n-1)%26)+ result
+            n = (n-1)/26
             
+        return result;
