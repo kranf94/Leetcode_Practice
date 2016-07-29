9adb2e5d9fe4450cd2753bb2b3273f3239a1c396
ppstacy <ppstacy@gmail.com>
2016-07-29 12:30:52 -0700

    [342. Power of Four][Accepted]committed by LeetCode Extension

---
+++ 342-Power-of-Four/solution2.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def isPowerOfFour(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        return num  > 0 and 4 ** round(math.log(num,4))==num
