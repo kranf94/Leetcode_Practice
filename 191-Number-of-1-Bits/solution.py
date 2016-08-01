2cd1f69f8452844ca9d4b07fbd95412d78d40135
ppstacy <ppstacy@gmail.com>
2016-08-01 10:19:10 -0700

    [191. Number of 1 Bits][Time Limit Exceeded]committed by LeetCode Extension

--- 191-Number-of-1-Bits/solution.py
+++ 191-Number-of-1-Bits/solution.py
@@ -6,7 +6,7 @@ class Solution(object):
         """
         num = '{0:b}'.format(n)
         count = 0
-        for i in len(num):
-            if num[i] == 1:
-                count +=1
+        for i in range(0, len(num)):
+            while num[i] == '1':
+                count = count +1
         return count
