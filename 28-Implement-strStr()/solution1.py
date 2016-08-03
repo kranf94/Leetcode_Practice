4ba12b0c5e061ea8f452c9ae682d0a0c8741f44c
ppstacy <ppstacy@gmail.com>
2016-08-03 13:14:53 -0700

    [28. Implement strStr()][Accepted]committed by LeetCode Extension

---
+++ 28-Implement-strStr()/solution1.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        if haystack is None or needle is None:
+            return -1
+        return haystack.find(needle)
