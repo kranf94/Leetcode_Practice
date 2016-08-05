5e34f8cf9c99df682e4cb2b790702980f70b6f62
ppstacy <ppstacy@gmail.com>
2016-08-05 10:17:13 -0700

    [90. Subsets II][Runtime Error]committed by LeetCode Extension

--- 90-Subsets-II/solution.py
+++ 90-Subsets-II/solution.py
@@ -13,5 +13,5 @@ class Solution(object):
     def search(self, nums, path, index, results):
         if path not in results:
             results.append(path)
-        for i range(index, len(nums)):
+        for i in range(index, len(nums)):
             self.search(nums, path+[nums[i]], i+1 ,results)
