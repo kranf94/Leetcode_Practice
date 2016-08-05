c089d55b9adc4d19a675f35696b6013097eec4e2
ppstacy <ppstacy@gmail.com>
2016-08-05 10:16:59 -0700

    [90. Subsets II][Runtime Error]committed by LeetCode Extension

---
+++ 90-Subsets-II/solution.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def subsetsWithDup(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        results = []
+        if len(nums) == 0:
+            return results
+        self.dfs(nums, [] , 0 ,results)
+        return results
+    
+    def search(self, nums, path, index, results):
+        if path not in results:
+            results.append(path)
+        for i range(index, len(nums)):
+            self.search(nums, path+[nums[i]], i+1 ,results)
